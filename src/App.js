923462e154a6c7127debb324c569feeecc7755bb
Trần Việt Tùng <tranviettung369@gmail.com>
2020-04-28 11:31:41 +0700

    finish fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import {Cards, Chart, CountryPicker} from './Components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+
 class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
     render() {
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                {/* this makes sure that you don't have any interference another other css file across your whole file system */}
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         );
     }
